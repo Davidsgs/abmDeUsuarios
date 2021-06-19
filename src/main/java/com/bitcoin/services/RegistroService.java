7f191c8713ab363d8edc35c3aaedd5d4cc822389
DavidG <65723994+Davidsgs@users.noreply.github.com>
2021-06-19 19:06:27 -0300

    Updated Service / updateMethod

--- src/main/java/com/bitcoin/services/RegistroService.java
+++ src/main/java/com/bitcoin/services/RegistroService.java
@@ -5,7 +5,7 @@ import com.bitcoin.models.Registro;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
