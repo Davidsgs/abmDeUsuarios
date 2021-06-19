0576aab541880b009c9d70c6b54e25478259b282
David Garcia <davidsamuelgarciasanchez@gmail.com>
2021-06-19 18:43:53 -0300

    Se acomodo problema con el Cors.

--- src/main/java/com/bitcoin/config/ApplicationConfig.java
+++ src/main/java/com/bitcoin/config/ApplicationConfig.java
@@ -5,10 +5,14 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Collection;
 import java.util.Collections;
